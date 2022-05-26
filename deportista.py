3370e6bacdd0062df89d8d9fa428835954feda02
Jhiguitam <jhiguitam@unal.edu.co>
2022-05-25 23:01:59 -0500

    py

---
+++ deportista.py
@@ -0,0 +1,20 @@
+from persona import Persona
+
+
+class Deportista(Persona):
+    def __init__(self, nombre, edad, altura, sexo, deporte, anos):
+        super().__init__(nombre, edad, altura, sexo)
+        self._deporte = deporte
+        self._anosPracticando = anos
+
+    def getDeporte(self):
+        return self._deporte
+
+    def getAñosPracticando(self):
+        return self._anosPracticando
+
+    def setDeporte(self, nuevo):
+        self._deporte = nuevo
+
+    def setAñosPracticando(self, anos):
+        self._anosPracticando = anos
